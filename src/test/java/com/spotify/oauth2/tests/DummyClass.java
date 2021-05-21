ded683e297dfea11fbb5f327c4a546b787da0666
Neehal Chowdekar <neehal@lenskart.in>
2021-05-21 23:09:55 +0530

    Added dummy method

--- src/test/java/com/spotify/oauth2/tests/DummyClass.java
+++ src/test/java/com/spotify/oauth2/tests/DummyClass.java
@@ -1,5 +1,12 @@
 package com.spotify.oauth2.tests;
 
+import org.testng.annotations.Test;
+
 public class DummyClass {
+	
+	@Test
+	public void dumyMethod() {
+		System.out.println("Dummy Method");
+	}
 
 }
